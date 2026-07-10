1cf0b80912e95d64d29ac9b7dbd9e79fb8c36864
Mahitha Thippireddy <mahithat@amazon.com>
2026-07-10 12:32:14 -0700

    chore: Rename to Trivi-Yuh

--- server.js
+++ server.js
@@ -485,5 +485,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Parlay All Day running on http://localhost:${PORT}`);
+  console.log(`Trivi-Yuh running on http://localhost:${PORT}`);
 });
